cee8b1cfb16a56f43d45f410286ee01f9e9faf7b
vijitha.madurasinghe@gmail.com <vijitha.madurasinghe@gmail.com>
2021-05-06 23:51:52 +0530

    finish: Intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,22 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import {useEffect, useRef} from "react"; 
+
 
 export default function Intro() {
+    const textRef = useRef();
+    
+    useEffect(() => {
+          init(textRef.current, 
+            {
+                showCursor: true, 
+                typeSpeed:  120,
+                backDelay:1600,
+                backSpeed:50,
+                strings: ['Developer', 'Designer'] 
+            });
+    }, [])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +28,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Vijitha Mahesh</h1>
-                    <h3>Professional<span></span></h3>
+                    <h3>Professional <span ref={textRef}/></h3> 
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt=""/>
