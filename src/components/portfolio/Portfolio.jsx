70d49550bd7c8013a3ea95049a7d171a5221e9c2
vijitha.madurasinghe@gmail.com <vijitha.madurasinghe@gmail.com>
2021-05-07 00:36:47 +0530

    develop: initialize portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,41 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>UI/UX</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+                <div className="item">
+                    <img src="https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg" alt="" />
+                    <h3>WATERrhythem</h3>
+                </div>
+            </div>
+
         </div>
     )
 }
