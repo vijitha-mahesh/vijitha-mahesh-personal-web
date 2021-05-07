09570f58f4e1b7d8c0dbc89017fc32bf4d8f29ea
vijitha.madurasinghe@gmail.com <vijitha.madurasinghe@gmail.com>
2021-05-07 14:07:35 +0530

    develop: basic structure of works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,29 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                    Reiciendis debitis autem sint animi numquam sed repellat.
+                                </p>
+                                <span>Project</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="https://wpuploads.appadvice.com/wp-content/uploads/2014/05/IMG_8035-576x1024.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" />
+            <img src="assets/arrow.png" className="arrow right" alt="" />
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,166 @@
+export const featuredPortfolio = [
+{
+    id:1,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:2,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:3,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:4,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:5,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:6,
+    title:"feature title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+];
+
+export const webPortfolio = [
+{
+    id:1,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+{
+    id:2,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+{
+    id:3,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+{
+    id:4,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+{
+    id:5,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+{
+    id:6,
+    title:"web title",
+    img:"https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg"
+},
+];
+
+
+export const mobilePortfolio = [
+{
+    id:1,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:2,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:3,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:4,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:5,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:6,
+    title:"mobile title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+];
+
+
+export const uiPortfolio = [
+{
+    id:1,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:2,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:3,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:4,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:5,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:6,
+    title:"ui title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+];
+
+export const graphicPortfolio = [
+{
+    id:1,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:2,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:3,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:4,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:5,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+{
+    id:6,
+    title:"graphic title",
+    img:"https://cdn57.androidauthority.net/wp-content/uploads/2016/04/Google-Travel-app-beta.jpg"
+},
+];
