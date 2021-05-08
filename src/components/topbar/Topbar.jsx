938eb2341f252130a079c110555f995fce69b608
vijitha.madurasinghe@gmail.com <vijitha.madurasinghe@gmail.com>
2021-05-08 21:53:28 +0530

    add: cv download button

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,30 +1,36 @@
-import "./topbar.scss"
-import {Person, Mail} from "@material-ui/icons" 
+import "./topbar.scss";
+import { Person, Mail, Pages } from "@material-ui/icons";
 
-
-export default function Topbar({menuOpen,setMenuOpen}) {
-    return (
-        <div className={"topbar " + (menuOpen && "active")}>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">.VMM </a>
-                    <div className="itemContainer">
-                        <Person className="icon"/>
-                        <span>+94 77 560 7334</span>
-                    </div>
-                     <div className="itemContainer">
-                        <Mail className="icon"/>
-                        <span>vijithamaheshp@gmail.com</span>
-                    </div>
-                </div>
-                <div className="right">
-                    <div class="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
-            </div>
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            .VMM{" "}
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+94 77 560 7334</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>vijithamaheshp@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <button href="assets/VijithaMahesh_CV.pdf" download>
+              download my CV
+            </button>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div class="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
